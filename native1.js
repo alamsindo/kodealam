3b8868f0455273829adb4c99616bef0f8b495686
alamsindo <99376399+alamsindo@users.noreply.github.com>
2022-02-10 11:29:08 +0700

    Delete native1.js

--- native1.js
+++
@@ -1,9 +0,0 @@
-
-	atOptions = {
-		'key' : 'b80f4dcfce6bea2b8fcb00408b16ac9e',
-		'format' : 'iframe',
-		'height' : 300,
-		'width' : 160,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://padsims.com/b80f4dcfce6bea2b8fcb00408b16ac9e/invoke.js"></scr' + 'ipt>');
